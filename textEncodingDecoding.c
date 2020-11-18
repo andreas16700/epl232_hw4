2d198ee9795d1d638a887aea1b8db7bb22dec423
sohanassa <snassa01@ucy.ac.cy>
2020-11-18 23:04:33 +0200

    created createPermutationFunction

--- textEncodingDecoding.c
+++ textEncodingDecoding.c
@@ -2,7 +2,8 @@
 // Created by sohae on 18/11/2020.
 //
 #include "bmplib.h"
-#include<string.h>
+#include <string.h>
+#include <stdlib.h>
 
 newImage encodeTextInsideAnImage(FILE *sourceImage, FILE *textToHide) {
 
@@ -21,6 +22,25 @@ PRIVATE int getBit(char *m, int n) {
     if (found > 0)
         return 1;
     return 0;
+}
+
+PRIVATE int *createPermutationFunction(int N, unsigned int systemkey) {
+    srand(systemkey);
+    //creates array of permutations
+    int *perm = (int *) malloc(N * sizeof(int));
+    //initializes with i value
+    for (int i = 0; i < N; i++) {
+        perm[i] = i;
+    }
+    //randomly chanmges order
+    for (int i = 0; i < N; i++) {
+        int val1 = rand() % N;
+        int val2 = rand() % N;
+        int temp = perm[val1];
+        perm[val1] = perm[val2];
+        perm[val2] = temp;
+    }
+    return perm;
 }
 
 #define DEBUG_TEXTENCODINGDECODING
