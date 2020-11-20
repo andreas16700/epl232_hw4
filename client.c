856502ff852c65ab36d7940eb58243639fbb2604
sohanassa <snassa01@ucy.ac.cy>
2020-11-20 22:15:17 +0200

    client main cleaned

--- client.c
+++ client.c
@@ -4,11 +4,7 @@
 
 #include "bmplib.h"
 #include <stdio.h>
+#include<string.h>
+
 int main(int argc, char *argv[]) {
-    //printf("hello ");
-    getMetaInfo("tux-pirate.bmp");
-    encodeTextInsideAnImage("tux-pirate.bmp", "input.txt", 5);
-    printf("hi\n");
-    //char *txt = decodeTextFromImage("new-tux-pirate.bmp",5,112);
-    //printf("%s\n",txt);
 }
