c34abd8163b23f7816f410b3ce5f99cef8764b0e
Andreas Loizides <andreas16700@icloud.com>
2020-11-21 14:31:03 +0200

    cleanup and add test func

--- imageInimageSteganography.c
+++ imageInimageSteganography.c
@@ -15,12 +15,13 @@
 
 
 PRIVATE byte mergeBytes(byte importantByte, byte lessImportantByte, int bitsToUse) {
-    if (bitsToUse > 7 || bitsToUse < 1)
-        exit(-1);
-
+    if (bitsToUse>7)
+        return lessImportantByte;
+    if (bitsToUse<1)
+        return importantByte;
     byte mask = (byte) 0;
     mask = ~mask;
-    //mask now is 8 consecutive zeroes
+    //mask now is 8 consecutive ones
     byte upperMask = mask >> (byte) bitsToUse;
     upperMask <<= (byte) bitsToUse;
     byte lowerMask = ~upperMask;
@@ -30,29 +31,9 @@ PRIVATE byte mergeBytes(byte importantByte, byte lessImportantByte, int bitsToUs
     byte lowerPart = lowerMask & lessImportantByte;
     result |= upperPart;
     result |= lowerPart;
-    if (DEBUG_MERGE) {
-    printf("\nUpper mask:\t");
-    printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(upperMask));
-    printf("\nLower mask:\t");
-    printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(lowerMask));
-    printf("\nImportant byte:\t%d\t", importantByte);
-    printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(importantByte));
-    printf("\nLess important byte:\t%d\t", lessImportantByte);
-    printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(lessImportantByte));
-    printf("\nResult:\t%d\t", result);
-    printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(result));
-    fflush(stdout);
-    }
     return result;
 }
-PRIVATE void copyHeader(FILE* from, FILE* to){
-    rewind(from);
-    rewind(to);
-    for (int i = 0; i < HEADER_BYTE_LENGTH; ++i) {
-        byte headerByte = fgetc(from);
-        fputc(headerByte,to);
-    }
-}
+
 PRIVATE byte extractHiddenImageByte(byte mergedByte, int bitsToUse){
     if (bitsToUse>7 || bitsToUse<1)
         exit(-1);
@@ -95,50 +76,31 @@ PRIVATE void writeMergedImageData(FILE* shellImage
         lessImportantByte = fgetc(secretImage);
     }
 }
-PRIVATE void appendBeforeExtension(char* slot, char* existing, char* toAppend){
-    slot[0]='\0';
-    int existingLength = strlen(existing);
-    int appendLength = strlen(toAppend);
-
-    int extensionLength = strlen(".xxx");
-    strcat(slot,existing);
-    slot[existingLength-extensionLength]='\0';
-    strcat(slot,toAppend);
-    int i = existingLength-extensionLength+appendLength;
-    int j;
-    for (j = 0; j < extensionLength; ++j) {
-        slot[i+j]=existing[existingLength-extensionLength+j];
-    }
-    slot[i+j]='\0';
-}
-PUBLIC FILE * encodeImageWithinImage(char *shellImageName, char *hiddenImageName, int bitsToUse){
-    FILE* shellImage = fopen(shellImageName,"r");
-    FILE* hiddenImage = fopen(hiddenImageName,"r");
-    ensureIsValidBMP(shellImage);
-    ensureIsValidBMP(hiddenImage);
-    char newName[strlen(shellImageName)+5];
-    appendBeforeExtension(newName,shellImageName,"-new");
-
-    FILE* output = fopen(newName, "w");
-    copyHeader(hiddenImage,output);
-    copyHeader(shellImage,output);
-    writeMergedImageData(shellImage,hiddenImage,output,bitsToUse);
-    fflush(output);
-    fclose(shellImage);
-    fclose(hiddenImage);
-    rewind(output);
-    return output;
-}
 PRIVATE void doneWithFile(FILE* file){
     fflush(file);
     fclose(file);
 }
-PUBLIC void decodeHiddenImageFromEncodedImage(char* fileNameOfImageWithHiddenImage, int bitsToUse){
-    FILE* imageWithHiddenImage = fopen(fileNameOfImageWithHiddenImage, "r");
+
+PUBLIC void encodeImageWithinImage(const char *shellImageName,const char *hiddenImageName, int bitsToUse){
+    FILE* shellImage = fopen(shellImageName,"rb");
+    FILE* hiddenImage = fopen(hiddenImageName,"rb");
+    ensureIsValidBMP(shellImage);
+    ensureIsValidBMP(hiddenImage);
+    char* newName = addPrefix(shellImageName,"new-");
+    FILE* output = fopen(newName, "wb");
+    copyHeader(hiddenImage,output);
+    copyHeader(shellImage,output);
+    writeMergedImageData(shellImage,hiddenImage,output,bitsToUse);
+    doneWithFile(shellImage);
+    doneWithFile(hiddenImage);
+    doneWithFile(output);
+}
+
+PUBLIC void decodeHiddenImageFromEncodedImage(const char* fileNameOfImageWithHiddenImage, int bitsToUse){
+    FILE* imageWithHiddenImage = fopen(fileNameOfImageWithHiddenImage, "rb");
     ensureIsValidBMP(imageWithHiddenImage);
-    char newName[strlen(fileNameOfImageWithHiddenImage)+5];
-    appendBeforeExtension(newName,fileNameOfImageWithHiddenImage,"-new");
-    FILE* extractedImage = fopen(newName,"w");
+    char* newName = addPrefix(fileNameOfImageWithHiddenImage,"new-");
+    FILE* extractedImage = fopen(newName,"wb");
     copyHeader(imageWithHiddenImage,extractedImage);
     extractImageData(imageWithHiddenImage,extractedImage,bitsToUse);
     doneWithFile(imageWithHiddenImage);
@@ -146,7 +108,33 @@ PUBLIC void decodeHiddenImageFromEncodedImage(char* fileNameOfImageWithHiddenIma
 }
 
 #ifdef ENCODEIMAGE_DEBUG
+#include <time.h>
+#include <assert.h>
+
+void testMergeByte(){
+    srand48(time(NULL));
+    byte b1 = (unsigned)lrand48()&(unsigned)255;
+    byte b2 = (unsigned)lrand48()&(unsigned)255;
+    int bitsToUse = (signed)((unsigned) lrand48()&(unsigned)7);
+    int diff = 8-bitsToUse;
+//    byte lsb
+
+    printf("Visual test:\n");
+    printf("\t\t\t\t\t\tdec\tbinary\n");
+    for(int i = 0; i<=8; i++) {
+        printf("Bits used: %d",i);
+        printf("\nImportant byte\t\t:\t%d\t", b1);
+        printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(b1));
+        printf("\nLess important byte\t:\t%d\t", b2);
+        printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(b2));
+        byte result = mergeBytes(b1,b2,i);
+        printf("\nResult\t\t\t\t:\t%d\t", result);
+        printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(result));
+        printf("\n\n");
+        fflush(stdout);
+    }
+}
 int main(){
-    //test here
+    testMergeByte();
 }
 #endif
