3fb79d476b210069de62652be998e2c3843d1720
sohanassa <snassa01@ucy.ac.cy>
2020-11-22 22:03:09 +0200

    fixed metaInfo which werked b4 u fucked it :(

--- metaInfo.c
+++ metaInfo.c
@@ -7,7 +7,7 @@
 PUBLIC void getMetaInfo(char *bmpImage) {
     byte *data = readImage(bmpImage);
     printf("BITMAP_FILE_HEADER\n"
-           "==================");
+           "==================\n");
     printf("bfType: %c%c\n", data[0], data[1]);
 
     printf("bfSize: %lu\n", getLongFrom4Bytes(&data[2]));
@@ -16,12 +16,12 @@ PUBLIC void getMetaInfo(char *bmpImage) {
     printf("bfOffBits: %lu\n", getLongFrom4Bytes(&data[10]));
     printf("\n");
     printf("BITMAP_INFO_HEADER\n"
-           "==================");
+           "==================\n");
     printf("biSize: %lu\n", getLongFrom4Bytes(&data[14]));
     printf("biWidth: %lu\n", getLongFrom4Bytes(&data[18]));
     printf("biHeight: %lu\n", getLongFrom4Bytes(&data[22]));
-    printf("biPlanes: %u\n");
-    printf("biBitCount: %u\n");
+    printf("biPlanes: %u\n",getLongFrom2Bytes(&data[26]));
+    printf("biBitCount: %u\n",getLongFrom2Bytes(&data[28]));
     printf("biCompression: %lu\n", getLongFrom4Bytes(&data[30]));
     printf("biSizeImage: %lu\n", getLongFrom4Bytes(&data[34]));
     printf("biXPelsPerMeter: %lu\n", getLongFrom4Bytes(&data[38]));
